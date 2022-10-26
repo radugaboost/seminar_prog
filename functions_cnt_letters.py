29391015dac4eee6c40766f5acae8022cedda0af
kirillsev1 <kirillprih47@gmail.com>
2022-10-26 14:30:32 +0300

    try

--- functions_cnt_letters.py
+++ functions_cnt_letters.py
@@ -1,3 +1,18 @@
+import time
+
+def worktime(f1, f2):
+    def f(string):
+        initial_time = time.time()
+        f1(string)
+        first = time.time() - initial_time
+        initial_time = time.time()
+        f2(string)
+        second = time.time() - initial_time
+        print(f1.__name__ if first < second else f2.__name__)
+    return f
+
+
+
 def double_letter(string):
     s = set()
     for i in string:
@@ -7,3 +22,7 @@ def double_letter(string):
 
 def count_duplicates(string):
     return len([ch for ch in (set(string.lower())) if string.lower().count(ch)>1])
+
+
+funcion = worktime(count_duplicates, double_letter)
+funcion('aaaBBnn')
